31b7de795f542d80ad3530666d8edfc3d43509be
Ong Yong Xin <ongyongxin267@gmail.com>
2019-10-24 08:31:30 +0800

    Add files via upload

---
+++ LATTE_CHANGELOG.md
@@ -0,0 +1,18 @@
+# Latte Changelog
+
+## 2.0.0-alpha
+- Joining file paths now uses `os.path.join()`.
+- `ROOT` variable is now set as `os.path.expanduser("~")`.
+- Repos can now be removed with `latte del-repo nickname`.
+- The `stansi` class for colours now has been split into two `ansi` colours, one for StaSh and another for LibTerm.
+- New function, `onstash()`, to check if Latte is running in a StaSh instance.
+- New function, `init()`, to create paths required by Latte.
+- The code now (mostly) conforms to PEP8 (except in areas where it is not better to do so, obviously.)
+- The "universe" (i.e, default) repository has been change to [this](https://github.com/sn3ksoftware/lattepkgs/master) repo.
+- Better exception handling (Errors are prettier!)
+- `latte list-repos nickname` now shows the URL of the repo under that nickname.
+- To update Latte, you can just do `latte install latte`.
+- Yellow colours for `WARNING` level alerts.
+
+## 1.0.0-alpha
+- Initial fork from the main Latte repo (Seanld/Latte).
--- latte.py
+++ latte.py
@@ -1,189 +1,416 @@
-# A package manager meant for Pythonista, built on StaSh.
+# -*- coding: utf-8 -*-
+"""
+A package manager meant for Pythonista, built on StaSh.
+# Code copyright (c) Seanld/sn3ksoftware 2017-2019, MIT License.
+# This is a fork by sn3ksoftware to enable
+compatibility with other platforms (i.e Libterm.)
+"""
+from __future__ import print_function
+
+import argparse
+import os
+import shutil
+import sys
+
+from os.path import join
 
 import requests
+
+
+__version__ = "2.0.0-alpha"
+__author__ = "Seanld/sn3ksoftware"
+__copyright__ = "Copyright (c) Seanld/sn3ksoftware 2017-2019, MIT License."
+desc = f"apt-get for Pythonista, version {__version__}.\n{__copyright__}"
+
+
+cwd = os.getcwd()
+ROOT = os.path.expanduser("~/Documents")
+SWPATH = join(ROOT, ".latte-repos.swconf")
+LATTEPATH = join(ROOT, "stash_extensions", "latte")
+BINPATH = join(ROOT, "bin")
+
+
+# Placeholder code for making a new package
+dev_template = """
+# This is just an example template. You can change this all you like.
+
 import sys
 import argparse
-from os import remove, mkdir, rename, listdir, getcwd
-from shutil import rmtree
-
-cwd = getcwd()
-documentsIndex = cwd.index("Documents")
-documentsIndex += len("Documents")
-ROOT = cwd[:documentsIndex]
-
-class stansi: # Collection of Stash's ANSI escape codes.
-	bold = u"\033[1m"
-	underscore = u"\x9b4m"
-	attr_end = u"\x9b0m"
-	
-	fore_red = u"\x9b31m"
-	fore_green = u"\x9b32m"
-	fore_brown = u"\x9b33m"
-	fore_blue = u"\x9b34m"
-	fore_pink = u"\x9b35m"
-	fore_cyan = u"\x9b36m"
-	fore_white = u"\x9b37m"
-	fore_end = u"\x9b39m"
-	
-	back_red = u"\x9b41m"
-	back_green = u"\x9b42m"
-	back_brown = u"\x9b43m"
-	back_blue = u"\x9b44m"
-	back_pink = u"\x9b45m"
-	back_cyan = u"\x9b46m"
-	back_white = u"\x9b47m"
-	back_end = u"\x9b49m"
-	
-def Red(text):
-	return stansi.fore_red+text+stansi.fore_end
-def Blue(text):
-	return stansi.fore_blue+text+stansi.fore_end
-def Green(text):
-	return stansi.fore_green+text+stansi.fore_end
-def Cyan(text):
-	return stansi.fore_cyan+text+stansi.fore_end
-def Success(text):
-	return stansi.fore_green+stansi.bold+text+stansi.attr_end
-def Error(text):
-	return stansi.fore_red+stansi.bold+text+stansi.attr_end
-		
-class SWConfig (object): # Parser for the config files such as the repository listing.
-	def __init__(self, content):
-		self.data = {}
-		for line in content.splitlines():
-			key = line.split("=")[0]
-			value = line.split("=")[1]
-			self.data[key] = value
-			
-	def __getitem__(self, key):
-		return self.data[key]
-		
-	def keys(self):
-		return self.data.keys()
-		
-def remove_line(content, lines_list):
-	pointer = 0
-	for line in lines_list:
-		if content in line:
-			lines_list.pop(pointer)
-		pointer += 1
-	return lines_list
-		
-def download_package(url, package_name): # Handles the installation of packages directories (since they're no longer tarfiles)
-	content_listing = ["bin.py", "meta.latte"]
-	mkdir(ROOT+"/"+package_name)
-	for item in content_listing:
-		requested = requests.get(url+"/"+package_name+"/"+item)
-		content = requested.text
-		requested.close()
-		if content == "404: Not Found\n":
-			print(Error("ERROR") + ": Package not found.")
-			sys.exit()
-		opened = open(ROOT+"/"+package_name+"/"+item, "w")
-		opened.write(content)
-		opened.close()
 
 def main(sargs):
-	parser = argparse.ArgumentParser()
-	parser.add_argument("method", help="What action to perform (install, remove, etc)", type=str)
-	parser.add_argument("package", help="Name of package", type=str)
-	args = parser.parse_args(sargs)
-	
-	try:
-		opened = open(".latte-repos.swconf", "r")
-		opened.close()
-	except:
-		opened = open(".latte-repos.swconf", "w")
-		print(Error("WARNING") + ": Repository cache doesn't exist, rebuilding to default...")
-		opened.write("universe=https://raw.githubusercontent.com/Seanld/latte-universe/master")
-		opened.close()
-	
-	repo_listing_opened = open(".latte-repos.swconf", "r")
-	listing_content = repo_listing_opened.read()
-	repo_listing_opened.close()
-	REPOSITORIES = SWConfig(listing_content)
-		
-	if args.method == "install":
-		packageSplitted = args.package.split("/")
-		try:
-			package_name = packageSplitted[1]
-			repo_to_use = REPOSITORIES[packageSplitted[0]]
-		except IndexError:
-			
-			repo_to_use = REPOSITORIES["universe"]
-			package_name = packageSplitted[0]
-		print(Error("WARNING") + ": No repository specified, using universe by default...")
-		try:
-			download_package(repo_to_use, package_name)
-		except:
-			print(Error("ERROR") + ": Failed to find package")
-			sys.exit()
-		# Move to correct locations
-		print("Installing")
-		try:
-			rename(ROOT+"/"+package_name+"/meta.latte", ROOT+"/stash_extensions/latte/"+package_name+".latte")
-		except:
-			mkdir(ROOT+"/stash_extensions/latte")
-			rename(ROOT+"/"+package_name+"/meta.latte", ROOT+"/stash_extensions/latte/"+package_name+".latte")
-		rename(ROOT+"/"+package_name+"/bin.py", ROOT+"/stash_extensions/bin/"+package_name+".py")
-		rmtree(ROOT+"/"+package_name)
-		print(Success("SUCCESS") + ": Package '"+package_name+"' successfully installed!")
-	elif args.method == "remove":
-		try:
-			remove(ROOT+"/stash_extensions/bin/"+args.package+".py")
-			remove(ROOT+"/stash_extensions/latte/"+args.package+".latte")
-		except:
-			print(Error("ERROR") + ": Couldn't remove package; not found in resources.")
-			sys.exit()
-		print(Success("SUCCESS") + ": '"+args.package+"' removed!")
-	elif args.method == "update":
-		print("Jeez! Sorry, but we are currently working on self-update capabilities. For now, just redo the install process to update.")
-	elif args.method == "new":
-		try:
-			mkdir(args.package)
-			config = open(args.package+"/meta.latte", "w")
-			config.write("developer=Your name here\ndescription=Enter description of your app here\nversion=0.1")
-			config.close()
-			index = open(args.package+"/bin.py", "w")
-			index.write("# This is just an example template. You can change this all you like.\n\nimport sys\nimport argparse\n\ndef main(sargs):\n\tparser = argparse.ArgumentParser()\n\tparser.add_argument('echo', help='What you want the command to echo back.')\n\targs = parser.parse_args(sargs)\n\t\n\tprint('Echoing back: '+args.echo)\n\nif __name__ == '__main__':\n\tmain(sys.argv[1:])")
-			index.close()
-			print(Success("SUCCESS") + ": Package '"+args.package+"' generated, check current working directory!")
-		except:
-			print(Error("ERROR") + ": Couldn't generate package; directory may already exist.")
-	elif args.method == "add-repo":
-		try:
-			request = requests.get(args.package+"/init.latte")
-			data = request.text
-			request.close()
-			data_org = SWConfig(data)
-			nickname = data_org["NICKNAME"]
-			repo_listing = open(".latte-repos.swconf", "a")
-			repo_listing.write("\n"+nickname+"="+args.package)
-			repo_listing.close()
-			print(Success("SUCCESS") + ": '"+nickname+"' added to repositories cache!")
-		except:
-			print(Error("ERROR") + ": Either repository doesn't exist, or does not contain an 'init.latte' file.")
-	elif args.method == "list-repos":
-		if args.package == "all":
-			opened = open(".latte-repos.swconf")
-			content = opened.read()
-			opened.close()
-			as_config = SWConfig(content)
-			for repo in as_config.keys():
-				print(Cyan(repo) + ": " + Green(as_config[repo]))
-	elif args.method == "remove-repo":
-		opened = open(".latte-repos.swconf", "r")
-		data = opened.read().splitlines()
-		opened.close()
-		
-		removed = remove_line(args.package, data)
-		opened = open(".latte-repos.swconf", "w")
-		opened.writelines(removed)
-		opened.close()
-		
-		print(Success("SUCCESS") + ": Removed '" + args.package + "' from repository cache!")
-	else:
-		print(Error("ERROR") + ": Unknown command '"+args.method+"'!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('echo', help='What you want the command to echo back.')
+    args = parser.parse_args(sargs)
+    
+    print('Echoing back: '+args.echo)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+"""
+meta_template = """
+developer=Your name here
+description=Enter description of your app here
+version=0.0.1
+"""
+
+
+def onstash():
+    """Check if Latte is running on StaSh."""
+    try:
+        globals()['_stash']
+    except KeyError:
+        return False
+    else:
+        return True
+
+if onstash():
+    class ansi:
+        """Collection of Stash's ANSI escape codes."""
+        bold = u"\033[1m"
+        underscore = u"\x9b4m"
+        attr_end = u"\x9b0m"
+        
+        fore_red = u"\x9b31m"
+        fore_yellow = u"\x9b51m"
+        fore_green = u"\x9b32m"
+        fore_brown = u"\x9b33m"
+        fore_blue = u"\x9b34m"
+        fore_pink = u"\x9b35m"
+        fore_cyan = u"\x9b36m"
+        fore_white = u"\x9b37m"
+        fore_end = u"\x9b39m"
+        
+        back_red = u"\x9b41m"
+        back_green = u"\x9b42m"
+        back_brown = u"\x9b43m"
+        back_blue = u"\x9b44m"
+        back_pink = u"\x9b45m"
+        back_cyan = u"\x9b46m"
+        back_white = u"\x9b47m"
+        back_end = u"\x9b49m"
+else:
+    class ansi:
+        """Collection of LibTerm's ANSI escape codes."""
+        bold = u"\u001b[1m"
+        italic = u"\u001b[3m"
+        underscore = u"\x9b4m"
+        attr_end = u"\u001b[0m"
+        # All 'bright' colours execpt for red and brown.
+        fore_red = u"\u001b[31m"
+        fore_yellow = u"\u001b[91m"
+        fore_green = u"\u001b[90m"
+        fore_brown = u"\u001b[33m"
+        fore_blue = u"\u001b[92m"
+        fore_pink = u"\u001b[93m"
+        fore_cyan = u"\u001b[94m"
+        fore_white = u"\u001b[30m"
+        fore_end = u"\u001b[0m"
+
+
+def Red(text):
+    return ansi.fore_red + text + ansi.fore_end
+
+
+def Blue(text):
+    return ansi.fore_blue + text + ansi.fore_end
+
+
+def Green(text):
+    return ansi.fore_green + text + ansi.fore_end
+
+
+def Cyan(text):
+    return ansi.fore_cyan + text + ansi.fore_end
+
+
+def Yellow(text):
+    return ansi.fore_yellow + text + ansi.fore_end
+
+
+class SWConfig(object):
+    """Parser for the config files such as the repository listing."""
+    def __init__(self, content):
+        self.data = {}
+        for line in content.splitlines():
+            if line == "":
+                continue
+            else:
+                key = line.split("=")[0]
+                value = line.split("=")[1]
+                self.data[key] = value
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def keys(self):
+        return self.data.keys()
+
+
+def init():
+    """Setup Latte files and dirs"""
+    req_paths = [LATTEPATH, BINPATH]
+    for path in req_paths:
+        if os.path.isdir(path):
+            continue
+        else:
+            try:
+                os.mkdir(path)
+            except Exception as e:
+                print(
+                    Red("ERROR") +
+                    f": Could not create {path}. Traceback: {str(e)}"
+                )
+            else:
+                continue
+
+
+def download_package(url, package_name):
+    """Handles the installation of packages
+    directories (since they're no longer
+    tarfiles)"""
+    content_listing = ["bin.py", "meta.latte"]
+    try:
+        os.mkdir(join(ROOT, package_name))
+    except OSError:
+        print(
+            Yellow("WARNING") +
+            ": Temporary folder for package already exists."
+        )
+    for item in content_listing:
+        requested = requests.get(url + "/" + package_name + "/" + item)
+        content = requested.text
+        if not requested:
+            # Delete the unused temp folder first
+            shutil.rmtree(join(ROOT, package_name))
+            sys.exit()
+        else:
+            opened = open(join(ROOT, package_name, item), "w")
+            opened.write(content)
+            opened.close()
+        requested.close()
+    return True
+
+
+def main(sargs):
+    init()
+    
+    parser = argparse.ArgumentParser(
+        description=desc
+    )
+    parser.add_argument(
+        "method",
+        help="install, remove, new, add-repo, list-repos, del-repo",
+        type=str
+    )
+    parser.add_argument(
+        "package",
+        help="Name of package/nickname of repo (i.e, universe, all)",
+        type=str
+    )
+    args = parser.parse_args(sargs)
+
+    try:
+        opened = open(SWPATH, "r")
+        opened.close()
+    except:
+        opened = open(SWPATH, "w")
+        print(
+            Yellow("WARNING") +
+            ": Repository listing doesn't exist, rebuilding to default..."
+        )
+        opened.write(
+            "universe=https://raw.githubusercontent.com/sn3ksoftware/latte-universe/master"
+        )
+        opened.close()
+
+    repo_listing_opened = open(SWPATH, "r")
+    listing_content = repo_listing_opened.read()
+    repo_listing_opened.close()
+    REPOSITORIES = SWConfig(listing_content)
+
+    if args.method == "install":
+        packageSplitted = args.package.split("/")
+        try:
+            package_name = packageSplitted[1]
+            repo_to_use = REPOSITORIES[packageSplitted[0]]
+        except IndexError:
+
+            repo_to_use = REPOSITORIES["universe"]
+            package_name = packageSplitted[0]
+            print(
+                Yellow("WARNING") +
+                ": No repository specified, using universe by default..."
+            )
+        try:
+            download_package(repo_to_use, package_name)
+        except requests.ConnectionError:
+            print(
+                Red("ERROR") +
+                ": Couldn't find package. Check your Internet connection?"
+            )
+            sys.exit()
+        except requests.HTTPError:
+            print(
+                Red("ERROR") +
+                ": Invaild HTTP response. Contact the server admin/repo owner."
+            )
+            sys.exit()
+        except Exception as e:
+            print(
+                Red("ERROR") +
+                ": Unhandled exception occured. Content: " +
+                e
+            )
+            sys.exit()
+        # Move to correct locations
+        print("Extracting package files...")
+        try:
+            os.rename(
+                join(ROOT, package_name, "meta.latte"),
+                join(LATTEPATH, package_name + ".latte")
+            )
+        except Exception as e:
+            print(
+                Red("ERROR") +
+                ": Could not move meta.latte to LATTEPATH. Error: {str(e)}"
+            )
+            sys.exit()
+        else:
+            pass
+        
+        try:
+            os.rename(
+                join(ROOT, package_name, "bin.py"),
+                join(BINPATH, package_name + ".py")
+            )
+        except Exception as e:
+            print(
+                Red("ERROR") +
+                ": Could not move bin.py to BINPATH. Exeception: {str(e)}"
+            )
+        else:
+            pass
+        
+        shutil.rmtree(join(ROOT, package_name))
+        print(
+            Green("SUCCESS") +
+            ": Package '" +
+            package_name +
+            "' successfully installed!"
+        )
+    elif args.method == "remove":
+        try:
+            os.remove(join(BINPATH, args.package + ".py"))
+            os.remove(join(LATTEPATH, args.package + ".latte"))
+        except FileNotFoundError:
+            print(
+                Red("ERROR") +
+                ": Couldn't remove package; Not found in registry."
+            )
+            sys.exit()
+        except Exception as e:
+            print(
+                Red("ERROR") +
+                f": Couldn't remove package. Exception: {str(e)}"
+            )
+            sys.exit()
+        print(Green("SUCCESS") + ": '" + args.package + "' removed!")
+    elif args.method == "new":
+        try:
+            os.mkdir(args.package)
+            config = open(join(args.package, "meta.latte"), "w")
+            config.write(meta_template)
+            config.close()
+            index = open(join(args.package, "bin.py"), "w")
+            index.write(dev_template)
+            index.close()
+            print(
+                Green("SUCCESS") +
+                ": Package '" +
+                args.package +
+                "' generated, check current working directory!"
+            )
+        except FileExistsError:
+            print(
+                Red("ERROR") +
+                ": Couldn't generate package; directory may already exist."
+            )
+        except Exception as e:
+            print(
+                Red("ERROR") +
+                ": Exception occured. Traceback: " +
+                str(e)
+            )
+    elif args.method == "add-repo":
+        try:
+            request = requests.get(args.package + "/init.latte")
+            data = request.text
+            request.close()
+            data_org = SWConfig(data)
+            nickname = data_org["NICKNAME"]
+            repo_listing = open(SWPATH, "a")
+            repo_listing.write("\n" + nickname + "=" + args.package)
+            repo_listing.close()
+            print(
+                Green("SUCCESS") +
+                ": '" +
+                nickname +
+                "' added to repositories!"
+                )
+        except:
+            print(
+                Red("ERROR") +
+                ": Either repository doesn't exist," +
+                "or does not contain an 'init.latte' file."
+            )
+    elif args.method == "list-repos":
+        if args.package == "all":
+            opened = open(SWPATH)
+            content = opened.read()
+            opened.close()
+            as_config = SWConfig(content)
+            for repo in as_config.keys():
+                print(Cyan(repo) + ": " + Green(as_config[repo]))
+        else:
+            # Search for repo nickname in swconfig
+            try:
+                repo_url = REPOSITORIES[args.package]
+            except KeyError:
+                print(
+                    Red("ERROR") +
+                    f": No repository with nickname {args.package}."
+                )
+            else:
+                print(Cyan(args.package) + ": " + Green(repo_url))
+    elif args.method == "del-repo":
+        # Check if nickname in swconfig
+        repo_d = REPOSITORIES.data
+        if args.package in repo_d:
+            # Pop out the nickname and write back
+            removed = repo_d.pop(args.package)
+            # Erase the file first
+            swfile_w = open(SWPATH, "w")
+            swfile_w.write("")
+            # Write back changed dict
+            swfile_a = open(SWPATH, "a")
+            for key, val in repo_d.items():
+                swfile_a.write("\n" + key + "=" + val)
+            swfile_a.close()
+            print(
+                Green("SUCCESS") +
+                ": Removed repo " +
+                Green(removed) +
+                " with nickname " +
+                Cyan(args.package) + "."
+            )
+        else:
+            print(
+                Red("ERROR") +
+                f": Repo with nickname {args.package} not found!"
+            )
+    else:
+        print(Red("ERROR") + ": Unknown command '" + args.method + "'!")
+
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
