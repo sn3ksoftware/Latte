485697a2ac5f7f11a79f219ecb6c17c1323bd714
Ong Yong Xin <ongyongxin267@gmail.com>
2019-10-24 08:29:47 +0800

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,13 +1,24 @@
+import os
 import requests
+
+BINPATH = os.path.join(os.path.expanduser("~"), "Documents", "bin")
 
 print("Downloading...")
 
-request = requests.get("https://raw.githubusercontent.com/Seanld/Latte/master/latte.py")
+request = requests.get("https://raw.githubusercontent.com/sn3ksoftware/Latte/master/latte.py")
 data = request.text
 
 print("Installing...")
 
-opened = open("stash_extensions/bin/latte.py", "w")
+try:
+    os.mkdir(BINPATH)
+except FileExistsError:
+    pass
+except Exception as e:
+    print("ERROR: Failed to create the bin folder.")
+    exit()
+
+opened = open(os.path.join(BINPATH, "latte.py"), "w")
 opened.write(data)
 opened.close()
 
